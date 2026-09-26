5b626e4d8dc4a38e88330aa1c4a5920c33315bfa
Reece O'Reilly <reecetyleroreilly@gmail.com>
2026-09-26 15:33:47 +0100

    After taking a break and looking at some Flutter Stuff i found out how change the alignment of the text while also changing how the contianer width and height

--- lib/views/movie_listing.dart
+++ lib/views/movie_listing.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:southsea_cinema/constants.dart';
 import 'package:southsea_cinema/widgets/nav_drawer.dart';
 
-
 class MovieListing extends StatelessWidget {
   const MovieListing({super.key});
 
@@ -16,41 +15,48 @@ class MovieListing extends StatelessWidget {
           elevation: 0,
         ),
         drawer: const NavDrawer(),
-        //body: const SizedBox.shrink(
+        //body: const SizedBox.shrink()
 
-        //)
+        body: Align(
+          alignment: Alignment.topLeft,
 
-        body: Container(
-            alignment: Alignment.topLeft,
-            //color: Colors.amber[50],
+            child: Container(
+              width: 600,
+              height:400,
+              child: Column(
 
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Title(color: Colors.black, child: const Text(
-                  'Iron Man 3',
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                )),
-                
-                const Text(
-                    'after his personal world is destroyed, Stark undertakes a challenging quest for those responsible, relying on his own ingenuity, instincts, and devices to survive and protect his loved ones. Along the way, he ultimately confronts the question that has long haunted him: whether the man makes the suit or the suit makes the man.'),
-                DropdownMenu<int>(
-                  initialSelection: 5,
-                  label: const Text('Number of People'),
-                  dropdownMenuEntries: [
-                    DropdownMenuEntry(value: 1, label: '1 Person'),
-                    DropdownMenuEntry(value: 2, label: '2 People'),
-                    DropdownMenuEntry(value: 3, label: '3 People'),
-                    DropdownMenuEntry(value: 4, label: '4 People'),
-                    DropdownMenuEntry(value: 5, label: '5 People')
-                  ],
-                ),
-                ElevatedButton(
-                  onPressed: () => print('Added to basket'),
-                  child: const Text('Add to Basket'),
-                ),
-              ],
-            )));
+                children: [
+                  Title(
+                      color: Colors.black,
+                      child: const Text(
+                        'Iron Man 3',
+                        style:
+                            TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                      )),
+
+                  const SizedBox(height: 20),
+
+                  const Text(
+                      'after his personal world is destroyed, Stark undertakes a challenging quest for those responsible, relying on his own ingenuity, instincts, and devices to survive and protect his loved ones. Along the way, he ultimately confronts the question that has long haunted him: whether the man makes the suit or the suit makes the man.'),
+                  DropdownMenu<int>(
+                    initialSelection: 5,
+                    label: const Text('Number of People'),
+                    dropdownMenuEntries: [
+                      DropdownMenuEntry(value: 1, label: '1 Person'),
+                      DropdownMenuEntry(value: 2, label: '2 People'),
+                      DropdownMenuEntry(value: 3, label: '3 People'),
+                      DropdownMenuEntry(value: 4, label: '4 People'),
+                      DropdownMenuEntry(value: 5, label: '5 People')
+                    ],
+                  ),
+                  ElevatedButton(
+                    onPressed: () => print('Added to basket'),
+                    child: const Text('Add to Basket'),
+                  ),
+                ],
+              ),
+            ),
+        )
+    );
   }
 }
