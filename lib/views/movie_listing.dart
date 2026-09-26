cd4d1843cb355d232d36d7cf70843de2005a13ea
Reece O'Reilly <reecetyleroreilly@gmail.com>
2026-09-26 13:31:13 +0100

    added other information and a drop down menu now im trying to make it into rows

--- lib/views/movie_listing.dart
+++ lib/views/movie_listing.dart
@@ -23,35 +23,54 @@ class MovieListing extends StatelessWidget {
         alignment: Alignment.topLeft,
         //color: Colors.amber[50],
         
+        padding: const EdgeInsets.all(8.0),
+
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
 
-          children: [
-            const Text('Iron Man 3'),
+          
 
-            const TextField(
- 
-              decoration: InputDecoration(
-                labelText: 'description here'
-              ),
-              
+          children:[
+            
+            
+            const Text('Iron Man 3', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),),
+            const Text('after his personal world is destroyed, Stark undertakes a challenging quest for those responsible, relying on his own ingenuity, instincts, and devices to survive and protect his loved ones. Along the way, he ultimately confronts the question that has long haunted him: whether the man makes the suit or the suit makes the man.'),
+                
+
+            DropdownMenu<int>(
+              initialSelection: 5,
+              label: const Text('Number of People'),
+
+              dropdownMenuEntries: [
+                DropdownMenuEntry(value: 1, label: '1 Person'),
+                DropdownMenuEntry(value: 2, label: '2 People'),
+                DropdownMenuEntry(value: 3, label: '3 People'),
+                DropdownMenuEntry(value: 4, label: '4 People'),
+                DropdownMenuEntry(value: 5, label: '5 People')
+              ],              
             )
           ],
 
+          
         )
+          
+          
 
-        //child: const Text('Iron Man 3')
+
+
+      )
+
         
         
-      ),
-      
-
-      
-      
-
-
-      
     );
+      
+
+      
+      
+
+
+      
+    
 
     
 
